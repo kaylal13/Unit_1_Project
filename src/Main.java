30d6f46cbb652707006466927eb1cc17a751055f
KaylaLam <kaylal197@nycstudents.net>
2025-09-29 08:46:12 -0400

    fixed variable

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
         System.out.println("For tips, each person pays: $"+newTip);
 
 
-        double totalAmount= totalBill+newTip;
+        double totalAmount= totalBill+newTotalTip;
         System.out.println("The total amount (bill and tips) is: $"+totalAmount);
 
 
